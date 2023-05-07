deeb5c6d11a7218d3e873c4e16bf0f5fd340ce84
Mark Anthony Clerigo <clerigom7@gmail.com>
2023-05-07 22:23:20 +0800

    vote

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require("express")
+var app = express();
+
+const http = require('http');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+// const PORT = process.end.PORT || 8000;
+
+const defURL = "http://localhost:8000/"
+
+app.use(express.static('public'));
+
+app.set("view engine", "ejs");
+
+app.get("/vote", function(req, res){
+res.render("pages/vote", {socketURL:defURL});
+});
+app.get("/result", function(req, res){
+res.render("pages/result", {socketURL:defURL});
+});
+
+io.sockets.on('connection', function(socket){
+	socket.on('vote_intel', function(vote){
+	io.emit('vote_intel', vote);
+	});
+
+	socket.on('vote_ryzen', function(vote){
+	io.emit('vote_ryzen', vote);
+	});
+});
+
+server.listen(8000);
+console.log("server is listening on port: 8000");
+
+
